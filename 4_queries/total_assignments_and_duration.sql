98ba73f77b9bfab45e169612573c219b4b8717d5
Samir Parab <samirparab@yahoo.com>
2019-11-12 18:50:46 +0000

    total assignments and duration sql modified.

--- 4_queries/total_assignments_and_duration.sql
+++ 4_queries/total_assignments_and_duration.sql
@@ -1,10 +1,11 @@
 SELECT assignments.day, COUNT(assignments.name) AS total_assignments, SUM(assignments.duration) AS total_duration_per_day
 FROM assignments
 GROUP BY assignments.day
-ORDER BY total_duration_per_day DESC;
+ORDER BY assignments.day;
 
-SELECT day, count(*) as number_of_assignments, sum(duration) as duration
-FROM assignments
-GROUP BY day
-ORDER BY day;
+
+
+
+
+
 
