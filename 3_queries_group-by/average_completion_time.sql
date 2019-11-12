1b5df79429bb0b19c0877d5def7f1f2333525243
Samir Parab <samirparab@yahoo.com>
2019-11-12 14:19:47 +0000

    teachers and assistance_requests tables created.

--- 3_queries_group-by/average_completion_time.sql
+++ 3_queries_group-by/average_completion_time.sql
@@ -1,4 +1,4 @@
-SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
 FROM students
 JOIN assignment_submissions ON student_id = students.id
 WHERE end_date IS NULL
